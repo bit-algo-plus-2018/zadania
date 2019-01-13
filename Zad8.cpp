1939af066e492fd90ec8d9145c00bdc9c0fb4acf
maciektr <maciek2tr99@gmail.com>
2019-01-13 22:36:54 +0100

    Update Zad8.cpp

--- Zad8.cpp
+++ Zad8.cpp
@@ -1,63 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-//Srednica drzewa
 using adjacency=vector<vector<int> >;
-int readInput(adjacency &tree);
+void readInput(adjacency &tree);
 
-int bfs(int s, adjacency &tree, int n){
-    vector<int> distance(n,-1);
-    distance[s]=0;
+int farthest(int start, adjacency &tree, bool print=false){
+    const int n = (int)tree.size();
+    vector<int> last(n,-1);
     queue<int> que;
-    que.push(s);
-
+    que.push(start);
     while(!que.empty()){
         int top = que.front();
         que.pop();
         for(auto i : tree[top]){
-            if(distance[i]!=-1)
+            if(last[i]!=-1)
                 continue;
-            distance[i]=distance[top]+1;
+            last[i] = top;
             que.push(i);
         }
-    }
-    int resId = s;
-    int resVal = 0;
-    for(int i=0; i<n; i++){
-        if(distance[i]>resVal){
-            resVal = distance[i];
-            resId = i;
+        if(que.empty()){
+            if(print){
+                int k = top;
+                while(k!=start){
+                    cout<<k+1<<endl;
+                    k = last[k];
+                }
+                cout<<start+1<<endl;
+            }
+            return top;
         }
     }
-    return resId;
+    return start;
 }
 
-bool print(int n, int endPath, adjacency &tree,vector<bool> &seen){
-    bool doPrint = (n==endPath);
-    for(auto i : tree[n]){
-        if(seen[i])
-            continue;
-        seen[i] = true;
-        doPrint = doPrint || print(i,endPath,tree, seen);
-    }
-    if(doPrint)
-        cout<<n+1<<endl;
-    return doPrint;
-}
-
-void solve(adjacency &tree, int n){
-    int beginPath = bfs(0,tree,n);
-    int endPath = bfs(beginPath,tree,n);
-    vector<bool> seen(n,false);
-    print(beginPath,endPath,tree,seen);
+void solve(adjacency &tree){
+    int beginPath = farthest(0,tree);
+    farthest(beginPath,tree,true);
 }
 
 int main(){
     adjacency tree;
-    int n = readInput(tree);
-    solve(tree,n);
+    readInput(tree);
+    solve(tree);
 }
 
-int readInput(adjacency &tree){
+void readInput(adjacency &tree){
     //wierzcholki numerowane od 1 do n
     tree.clear();
     tree.shrink_to_fit();
@@ -72,5 +58,4 @@ int readInput(adjacency &tree){
         tree[a].push_back(b);
         tree[b].push_back(a);
     }
-    return n;
 }
